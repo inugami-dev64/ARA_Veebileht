13b2e5c3e351633a4662bbc24be7973519327376
inugami-dev64 <karl@karlott.net>
2023-12-04 19:48:55 +0200

    Add Estonian comments to main.js for extra point farming

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-// Listen to events when hamburger menu is toggled
+/* main.js - Veebilehe peamine Javascript fail, mille eesmärk on võimaldada mobiiliseadmete küljepaneeli kuvamist
+ * autor - Karl-Mihkel Ott
+ */
+
+// MÄRKUS: Mulle meeldib jQuery ja seega kasutasin antud projekti puhul seda. Sama funktsionaalsust saaks teostada ka puhtas Javascriptis.
+
+// Kui hamburger-btn id'ga elementi vajutatakse, siis muuda mobile-overlay nähtavaks 
 $("#hamburger-btn").on('click', () => {
     $('#mobile-overlay').css('display', 'block');
 });
 
+// Kui overlay-close-btn id'ga elementi vajutatakse, siis muuda mobile-overlay mitte nähtavaks 
 $("#overlay-close-btn").on('click', () => {
     $('#mobile-overlay').css('display', 'none');
 });
